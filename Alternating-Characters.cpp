e7cf2f77275dd02f407cdf5d7fdaae81e6503b7a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-01-10 09:10:56 +0900

    Update Alternating-Characters.cpp

--- Alternating-Characters.cpp
+++ Alternating-Characters.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'alternatingCharacters' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
 int alternatingCharacters(string s)
 {
     int result = 0;
